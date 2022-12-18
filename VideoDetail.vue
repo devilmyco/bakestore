98d2b7b2e13f3f1d9ff225a968594137b60ead0b
devilmyco <116631734+devilmyco@users.noreply.github.com>
2022-12-19 00:51:41 +0800

    Add files via upload

---
+++ VideoDetail.vue
@@ -0,0 +1,516 @@
+<template>
+    <div>
+
+        <!-- <p>分類>分類>分類</p> -->
+        <p style="color:transparent;">{{ this.$route.params.id }}</p>
+
+        <div id="videoMain">
+            <div id="videoMainLeft">
+                <div id="videoMainLeftDiv">
+                    <div>
+                        <p class="nh1 nb">{{ item[0].video_name }}</p>
+                        <br>
+                        <p>上傳日期：{{ item[0].video_upload_date }}</p>
+                        <br>
+                    </div>
+                    <div>
+                        <iframe :src="item[0].vedio_url" title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+
+                    </div>
+                    <br><br><br><br><br>
+                    <!-- auther
+                    product_id
+                    product_name
+                    vedio_url
+                    video_class
+                    video_intro
+                    video_mouseon_img
+                    video_name
+                    video_title_img
+                    video_upload_date -->
+                    <div>
+                        <p class="nh2 nb">影片介紹</p>
+                        <br>
+                        <span>
+                            {{ item[0].video_intro }}
+                        </span>
+                    </div>
+                    <br><br><br><br><br>
+                    <!-- 相關書籍產品 -->
+                    <div id="relatedProductOutDiv">
+                        <p class="nh2">相關產品</p>
+                        <br>
+                        <div id="relatedProduct">
+                            <div class="relatedProductDiv">
+                                <a class="relatedProductTag_a" :href="('/#/home/product/' + this.$route.params.id)">
+                                    <div>
+                                        <div class="relatedProductImgDiv">
+                                            <img class="relatedProductImg"
+                                                :src="'http://127.0.0.1:3000/img/books/' + bookinfo[0].img_cover + '.png'"
+                                                alt="">
+                                        </div>
+                                        <div>
+                                            <p class="nb product_name">{{ bookinfo[0].product_name }}</p>
+                                            <p class="auther">作者：{{ bookinfo[0].auther }}</p>
+                                            <!-- <p>NT 500</p> -->
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+
+                    <div>
+
+                    </div>
+                    <br><br><br><br><br><br><br><br><br><br>
+                </div>
+            </div>
+            <div id="videoMainRight">
+                <div id="videoMainRightDiv">
+
+                    <div class="bgcF7F7F7" id="videoMainRightDivTop">
+                        <br>
+                        <p class="nh2 nb m10">其他推薦影片</p>
+
+                        <hr>
+                    </div>
+                    <div v-for="(item, index) in otherVideo" :key="index">
+                        <div class=" m20">
+                            <a v-on:click="getInfo" class="block"
+                                :href="('/#/home/videodetail/' + otherVideo[index].product_id)">
+                                <div class="flex">
+                                    <div class=""><img class="otherVideo"
+                                            v-bind:src="'http://127.0.0.1:3000/img/video/' + otherVideo[index].video_title_img + '.jpg'"
+                                            alt="">
+                                    </div>
+                                    <div class="bookinfomation">
+                                        <p class="nh3 nb">{{ otherVideo[index].video_name }}</p>
+                                        <br>
+                                        <p class="nh4 nb">{{ otherVideo[index].video_class }}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+import axios from "axios"
+
+export default {
+    data() {
+        return {
+            allvideo: '',
+            // imgSrc:require('../../static/img/video/v10201.jpg'),
+            otherVideo: '',
+            item: '',
+            bookinfo: '',
+            id: this.$route.params.id,
+
+
+        };
+    },
+    mounted() {
+        this.getInfo();
+
+        axios.get('http://127.0.0.1:3000/videotable').then(res => {
+            this.allvideo = res.data;
+            var a = res.data;
+
+            function shuffleArray(inputArray) {
+                inputArray.sort(() => Math.random() - 0.5);
+            }
+            shuffleArray(a);
+            this.otherVideo = a.slice(0, 5);
+            console.log(a);
+            console.log(this.otherVideo);
+            console.log(res.data);
+
+        }),
+            axios.get(`http://localhost:3000/videodetail${this.id}`).then(res => {
+                this.item = res.data;
+                // console.log(this.item);
+                // console.log(res.data);
+            }),
+            axios.get(`http://localhost:3000/productdetail${this.id}`).then(res => {
+                this.bookinfo = res.data;
+                // console.log(res.data);
+
+            })
+
+    },
+
+    methods: {
+        getInfo() {
+            
+            axios.get(`http://localhost:3000/videodetail${this.id}`).then(res => {
+                this.item = res.data;
+                // console.log(this.item);
+                // console.log(res.data);
+            }),
+            axios.get(`http://localhost:3000/productdetail${this.id}`).then(res => {
+                this.bookinfo = res.data;
+                // console.log(res.data);
+                
+            })
+            // this.$router.push(`${this.$route.params.id}`)
+
+        }
+
+
+    },
+
+    watch: {
+        // 監聽動態路由變化
+        $route(to) {
+            this.id = to.params.id;
+            this.$emit('change-page');
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+/* <共用CSS> */
+.nh1 {
+    font-size: 24px;
+}
+
+.nh2 {
+    font-size: 20px;
+}
+
+.nh3 {
+    font-size: 18px;
+}
+
+.nh4 {
+    font-size: 13px;
+}
+
+.nh5 {
+    font-size: 11px;
+}
+
+.nh6 {
+    font-size: 10px;
+}
+
+.nb {
+    font-weight: bolder;
+}
+
+.mrauto {
+    margin-right: auto;
+}
+
+.flex {
+    display: flex;
+}
+
+.m10 {
+    margin: 10px;
+}
+
+.m20 {
+    margin: 20px;
+}
+
+.bgcF7F7F7 {
+    background-color: #F7F7F7;
+}
+
+
+
+/* </共用CSS> */
+
+/* 歸零CSS */
+/* v2.0 | 20110126
+  http://meyerweb.com/eric/tools/css/reset/ 
+  License: none (public domain)
+*/
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+}
+
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+    display: block;
+}
+
+body {
+    line-height: 1;
+}
+
+ol,
+ul {
+    list-style: none;
+}
+
+blockquote,
+q {
+    quotes: none;
+}
+
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+    content: '';
+    content: none;
+}
+
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+
+/* /歸零CSS */
+/* 影片區內頁css */
+
+/* 內容左邊 */
+#videoMain {
+    margin: auto;
+    width: 1200px;
+    display: flex;
+}
+
+#videoMainLeft {
+    width: 814px;
+    /* background-color: rgb(216, 186, 146); */
+
+}
+
+#videoMainLeftDiv {
+    background-color: #fff;
+    width: 700px;
+    margin: 40px auto 40px;
+
+}
+
+
+#videoMainRight {
+    width: 386px;
+    /* background-color: rgb(222, 165, 165); */
+}
+
+iframe {
+    border-radius: 40px;
+    width: 700px;
+    height: 394px;
+}
+
+/* /內容左邊 */
+/* 內容右邊 */
+#videoMainRightDiv {
+    margin: 40px auto 40px;
+    background-color: #fff;
+    width: 350px;
+    height: 940px;
+    border-radius: 30px;
+    overflow: auto;
+    box-shadow: 3px 3px 5px #d0d0d0;
+    border: 1px solid rgb(202, 192, 192);
+
+}
+
+#videoMainRightDivTop {
+    border-radius: 30px 30px 0 0;
+}
+
+
+
+
+.otherVideo {
+    width: 130px;
+    height: 130px;
+    object-fit: cover;
+    border-radius: 7px;
+    border: 1px solid #EAEAEA;
+}
+
+/* 相關產品 */
+#relatedProductOutDiv {
+    background-color: #ffffff;
+    /* margin: 20px; */
+}
+
+#relatedProductOutDiv>p {
+    /* padding: 10px; */
+    font-weight: bolder;
+    /* background-color: yellowgreen; */
+}
+
+.relatedProductTag_a {
+    display: block;
+}
+
+.bookinfomation {
+    margin-left: 10px;
+}
+
+.relatedProductDiv {
+    background-color: #fff;
+    margin: 20px 0;
+    width: 160px;
+
+}
+
+.relatedProductImgDiv {
+    text-align: center;
+    border: 1px solid #EAEAEA;
+
+}
+
+.auther {
+    text-align: start;
+    margin-top: 10px;
+}
+.product_name{
+    margin-top: 10px;
+
+}
+
+.relatedProductImg {
+    width: 126px;
+    height: 180px;
+    object-fit: cover;
+}
+
+#relatedProduct {
+    display: flex;
+    justify-content: start;
+    /* background-color: #F7F7F7; */
+    overflow-x: auto;
+
+}
+
+
+
+/* /相關產品 */
+
+
+
+
+/* /內容右邊 */
+
+
+
+/* /影片區內頁css */
+</style>
---
+++ VideoPage.vue
@@ -0,0 +1,414 @@
+<template>
+    <div>
+        <router-view></router-view>
+        <div id="videoPage">
+
+            <!-- <p>分類>分類>分類</p> -->
+            <!-- <button @click="get()">获取user表数据</button> -->
+            <br><br><br><br>
+            <div id="videoPlace" class="flex" style="background-color:white;">
+
+                <div class="videoDiv flex" v-for="(item, index) in pageVideo" :key="index">
+                    <a class="block flex" :href="('/#/home/videodetail/' + pageVideo[index].product_id)">
+                        <div>
+                            <div class="videoImgOutDiv">
+                                <!-- <img class="videoImg" v-bind:src="allvideo[0].video_title_img" alt=""> -->
+                                <img class="videoImg"
+                                    v-bind:src="'http://127.0.0.1:3000/img/video/' + pageVideo[index].video_title_img + '.jpg'"
+                                    alt="">
+                            </div>
+                        </div>
+                        <div class="bookInfo">
+                            <p class="nb nh3">{{ pageVideo[index].video_name }}</p>
+                            <small>{{ pageVideo[index].video_class }}</small>
+                            <br>
+                            <small>上傳日期：{{ pageVideo[index].video_upload_date }}</small>
+                        </div>
+                    </a>
+                </div>
+
+
+
+
+
+
+
+            </div>
+            <br><br>
+            <hr>
+            <div id="pageButton">
+                <!-- <a href="#" class="previous round">&#8249;</a> -->
+                <!-- <a href="#" class="numberRound">1</a>
+                    <a href="#" class="numberRound">2</a>
+                    <a href="#" class="numberRound">3</a> -->
+                <!-- <a href="#" class="numberRound">3</a> -->
+                <!-- <a href="#" class="next round">&#8250;</a> -->
+                <button class="previous round">&#8249;</button>
+                <button @click="pageOne" class="numberRound">1</button>
+                <button @click="pageTwo" class="numberRound">2</button>
+                <!-- <button class="numberRound">3</button> -->
+                <button @click="nextPage" class="next round">&#8250;</button>
+                <!-- <label for=""><input type="button" value="123"></label> -->
+            </div>
+            <br><br>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import axios from "axios"
+
+export default {
+    data() {
+        return {
+            aa: 1,
+            allVideo: '',
+            pageVideo: '',
+            // imgSrc:require('../../static/img/video/v10201.jpg'),
+
+
+
+        };
+    },
+    mounted() {
+        axios.get('http://127.0.0.1:3000/videotable').then(res => {
+
+            this.allVideo = res.data;
+            this.pageVideo = res.data.slice(0, 15)
+            console.log(res.data);
+            // console.log(this.imgSrc);
+        })
+    },
+
+    methods: {
+        pageOne() {
+            // let aa = 
+            this.pageVideo = this.allVideo.slice(0, 15);
+            console.log(this.pageVideo);
+        },
+
+        pageTwo() {
+            let aa = this.allVideo.slice(15, 30);
+            console.log(this.pageVideo);
+            this.pageVideo = aa;
+        },
+        nextPage() {
+
+
+
+        }
+
+        // get() {
+        //     axios.get('http://127.0.0.1:3000/videotable').then(res => {
+        //         console.log(res.data[0]);
+        //     }).catch(err => {
+        //         console.log("获取数据失败" + err);
+        //     })
+        // }
+    }
+}
+</script>
+
+
+<style scoped>
+/* <共用CSS> */
+.nh1 {
+    font-size: 24px;
+}
+
+.nh2 {
+    font-size: 20px;
+}
+
+.nh3 {
+    font-size: 17px;
+}
+
+.nh4 {
+    font-size: 13px;
+}
+
+.nh5 {
+    font-size: 11px;
+}
+
+.nh6 {
+    font-size: 10px;
+}
+
+.nb {
+    font-weight: bolder;
+}
+
+.mrauto {
+    margin-right: auto;
+}
+
+.flex {
+    display: flex;
+}
+
+.m10 {
+    margin: 10px;
+}
+
+.m20 {
+    margin: 20px;
+}
+
+.bgcF7F7F7 {
+    background-color: #F7F7F7;
+}
+
+
+
+/* </共用CSS> */
+
+/* 歸零CSS */
+/* v2.0 | 20110126
+  http://meyerweb.com/eric/tools/css/reset/ 
+  License: none (public domain)
+*/
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+}
+
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+    display: block;
+}
+
+body {
+    line-height: 1;
+}
+
+ol,
+ul {
+    list-style: none;
+}
+
+blockquote,
+q {
+    quotes: none;
+}
+
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+    content: '';
+    content: none;
+}
+
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+
+/* /歸零CSS */
+
+/* 主要內容 */
+#videoPage {
+    width: 1200px;
+    /* background-color: rgb(193, 227, 124); */
+    margin: 0 auto;
+}
+
+#videoPlace {
+    justify-content: space-around;
+    flex-wrap: wrap;
+
+}
+
+.videoImgOutDiv {
+    width: 180px;
+    height: 140px;
+    overflow: hidden;
+    border-radius: 10px;
+    border: 1px solid #EAEAEA;
+
+
+}
+
+.videoImg {
+    width: 180px;
+    height: 140px;
+    object-fit: cover;
+
+}
+
+.videoDiv {
+    width: 360px;
+    height: 160px;
+    margin-top: 20px;
+
+}
+
+.videoDiv:hover .videoImg {
+    transform: scale(1.2);
+    transition: .5s;
+
+
+
+}
+
+
+/* /主要內容 */
+/* 換頁按鈕連結樣式 */
+#pageButton {
+    text-align: center;
+}
+
+
+
+#pageButton>button:hover,
+#pageButton>a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+.previous {
+    background-color: #f1f1f1;
+    color: black;
+}
+
+.next {
+    /* background-color: #04AA6D;
+    color: white; */
+}
+
+.pageNumber {
+    background-color: #04AA6D;
+    color: black;
+}
+
+.round {
+    text-decoration: none;
+    display: inline-block;
+    padding: 8px 16px;
+    border-radius: 150%;
+    border: transparent;
+}
+
+.numberRound {
+    text-decoration: none;
+    display: inline-block;
+    padding: 8px 14px;
+    border-radius: 150%;
+    margin: 0px 5px;
+    border: transparent;
+
+
+
+}
+
+.bookInfo {
+    margin-left: 10px;
+    line-height: 28px;
+}
+
+.bookInfo small{
+    color: var(--neutral-color);
+}
+
+
+
+
+/* /換頁按鈕連結樣式 */
+</style>
